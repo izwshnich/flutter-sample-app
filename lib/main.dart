b07caa46fba2713fdf66d30d73ce11c06effa6a2
sizawa <sizawa@folio-sec.com>
2022-02-07 12:09:52 +0900

    use effect

--- lib/main.dart
+++ lib/main.dart
@@ -34,47 +34,39 @@ class Counter extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _counter = useState(0);
+    final counter = useState(0);
+    final controller = useTextEditingController();
+    final update = useValueListenable(controller);
+
+    useEffect(() {
+      counter.value = update.text.length;
+    }, [update]);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(this.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              _counter.value.toString(),
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                controller: controller,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: '山田',
+                ),
+                maxLength: 10,
+              ),
+              Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text('${counter.value.toString()} / 10')),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _counter.value++,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
